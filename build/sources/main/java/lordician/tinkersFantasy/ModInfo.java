83ad2791916d2e1be2e8ce3c39d6808c4a153158
Lordician <lordician@hotmail.com>
2017-07-27 17:02:39 +0200

    Updated .gitignore to remove some unnecessary items from the github.

---
+++ build/sources/main/java/lordician/tinkersFantasy/ModInfo.java
@@ -0,0 +1,11 @@
+package lordician.tinkersFantasy;
+
+public class ModInfo {
+
+	public static final String MODID = "tinkersfantasy";
+	public static final String NAME = "Tinkers' Fantasy";
+	public static final String VERSION = "1.0";
+	public static final String DEPENDS = "required-after:mantle;"
+			+ "required-after:tconstruct@[1.11.2-2.7.0.26,);";
+
+}
---
+++ build/sources/main/java/lordician/tinkersFantasy/common/item/ItemModelProvider.java
@@ -0,0 +1,7 @@
+package lordician.tinkersFantasy.common.item;
+
+import net.minecraft.item.Item;
+
+public interface ItemModelProvider {
+	void registerItemModel(Item item);
+}
